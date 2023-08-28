f409985ad421bfd9f5a9f30800aa34c663d11ac2
Fgerh42 <77984302+Fgerh42@users.noreply.github.com>
2023-08-28 11:08:46 -0300

    Update MainActivity.kt

--- example/android/app/src/main/kotlin/com/github/chinloyal/pusher_client_example/MainActivity.kt
+++ example/android/app/src/main/kotlin/com/github/chinloyal/pusher_client_example/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.github.chinloyal.pusher_client_example
+package com.github.Fgerh42.pusher_client_example
 
 import androidx.annotation.NonNull
 import io.flutter.embedding.android.FlutterActivity
