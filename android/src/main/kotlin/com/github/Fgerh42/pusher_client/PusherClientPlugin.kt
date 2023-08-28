7b94766d93141e3bc2c28f1c68f3789e7f4d535e
Fgerh42 <77984302+Fgerh42@users.noreply.github.com>
2023-08-28 10:50:09 -0300

    Update PusherClientPlugin.kt

--- android/src/main/kotlin/com/github/Fgerh42/pusher_client/PusherClientPlugin.kt
+++ android/src/main/kotlin/com/github/Fgerh42/pusher_client/PusherClientPlugin.kt
@@ -1,7 +1,7 @@
-package com.github.chinloyal.pusher_client
+package com.github.Fgerh42.pusher_client
 
 import androidx.annotation.NonNull
-import com.github.chinloyal.pusher_client.pusher.PusherService
+import com.github.Fgerh42.pusher_client.pusher.PusherService
 
 import io.flutter.embedding.engine.plugins.FlutterPlugin
 import io.flutter.plugin.common.MethodCall
